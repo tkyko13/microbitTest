407022314dc4452630e016958eab6ff5ea8594b9
tkyko13 <>
2019-12-10 23:14:10 +0900

    first

--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% weight=70 icon="\uf075" color=#555555 block="コメント"
+namespace comment {
+  //% blockId=show_strings block="コメント %v"
+  export function noaction(text: string): void {}
+}
