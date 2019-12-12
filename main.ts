2a76cfcf059e790ea06d0be0994fb343faa34b2a
Takayuki Ono <tkyk.o13@gmail.com>
2019-12-12 16:49:28 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
-//% weight=70 icon="\uf075" color=#555555 block="コメント"
-namespace comment {
-    //% blockId=show_strings block="コメント %v"
-    export function noaction(text: string): void { }
-}
+
+let x = 2
+let y = 2
+basic.forever(function () {
+    led.unplot(x, y)
+    x += input.acceleration(Dimension.X) / 1000
+    y += input.acceleration(Dimension.Y) / 1000
+    x = Math.constrain(x, 0, 4)
+    y = Math.constrain(y, 0, 4)
+    led.plot(x, y)
+})
